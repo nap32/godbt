b84e7380bd6d09fc18dd35ddeb54dad93073c1a2
naptime <10912027+nap32@users.noreply.github.com>
2023-06-19 05:31:02 +0000

    Initial axum proof-of-concept.

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[allow(unused_imports)]
+
+use anyhow::Result;
+use axum::{
+    body::Body,
+    extract::{State, Extension, Query},
+    http::{Response, StatusCode},
+    routing::get,
+    routing::post,
+    response::IntoResponse,
+    Router,
+    Json,
+};
+use tokio::sync::Mutex;
+use serde_json::{Value, json};
+use serde::{Serialize, Deserialize};
+use std::sync::Arc;
+use mongodb::{options::ClientOptions, Client, Database, Collection};
+use mongodb::bson::doc;
+use mongodb::bson::oid::ObjectId;
+
+#[derive(Clone)]
+struct AppState {
+    db: Arc<Mutex<Database>>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Document {
+    #[serde(rename = "_id")] // Use MongoDB's default
+    id: ObjectId,
+    name: String,
+    description: String,
+}
+
+// For MongoDB errors
+#[derive(Debug, Serialize)]
+struct ErrorResponse {
+    message: String,
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client_options = ClientOptions::parse("mongodb://127.0.0.1:27017").await?;
+    let client = Client::with_options(client_options)?;
+    let db = client.database("traffic");
+    let shared_state = Arc::new(AppState{
+        db: Arc::new(Mutex::new(db))
+    });
+
+    let app = Router::new()
+        .route("/", get(get_text))
+        .route("/json", get(get_json))
+        .route("/healthcheck", get(handle_db_healthcheck))
+        .route("/db", get(handle_traffic))
+        .with_state(shared_state);
+
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
+    Ok(())
+}
+
+async fn get_text(
+    State(state): State<Arc<AppState>>,
+) -> &'static str {
+    "Hello, World!"
+}
+
+async fn get_json(
+    State(state): State<Arc<AppState>>,
+) -> Json<Value> {
+    Json(json!({ "data": 42 }))
+}
+
+async fn handle_db_healthcheck(
+    State(app_state): State<Arc<AppState>>,
+) -> impl IntoResponse {
+    match app_state.db.lock().await.list_collection_names(None).await {
+        Ok(_) => (StatusCode::OK, "Database is healthy"),
+        Err(_) => (StatusCode::SERVICE_UNAVAILABLE, "Database is down"),
+    }
+}
+
+
+async fn handle_traffic(
+    Query(query): Query<Document>,
+    State(app_state): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, impl IntoResponse> {
+    let collection : Collection<Document> = app_state.db.lock().await.collection("documents");
+    let filter = doc! {
+        "name": &query.name,
+        "description": &query.description,
+    };
+
+
+    match collection.find_one(filter, None).await {
+        Ok(document) => {
+            match document {
+                Some(doc) => Ok(Json(doc)),
+                None => {
+                    let error_response = ErrorResponse { message: "No matching document found".to_string() };
+                    Err((StatusCode::NOT_FOUND, Json(error_response)))
+                }
+            }
+        },
+        Err(e) => {
+            let error_response = ErrorResponse { message: e.to_string() };
+            Err((StatusCode::INTERNAL_SERVER_ERROR, Json(error_response)))
+        }
+    }
+}
